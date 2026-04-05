c36dae150b8b2d05956e23c6048a37a825bf919e
Ryma Djoudad <zenyod85@gmail.com>
2026-04-05 12:58:11 -0400

    Added test cases

--- test_rotation_utils.py
+++ test_rotation_utils.py
@@ -5,3 +5,23 @@ April 5th, 2026
 Test if rotation_utils.py works as intended
 """
 
+import pytest 
+from rotation_utils import adjust_rotation
+
+def test_hundred():
+    assert adjust_rotation(100) == 100
+
+def test_four_sixty():
+    assert adjust_rotation(460) == 100
+
+def test_eight_twenty():
+    assert adjust_rotation(820) == 100
+
+def test_negative_hundred():
+    assert adjust_rotation(-100) == 260
+
+def test_negative_four_sixty():
+    assert adjust_rotation(-460) == 260
+
+def test_negative_eight_twenty():
+    assert adjust_rotation(-820) == 260
